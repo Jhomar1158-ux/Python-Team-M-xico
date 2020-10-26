0ef6580f45a292bd1ff17d94954cfb6dbfdc1649
JhomarCristianElias <jhomar.astuyauri.h@uni.pe>
2020-10-25 22:00:25 -0500

    hola mundo

---
+++ C2.py
@@ -0,0 +1,73 @@
+#Tipo de dato: ENTERO 
+
+'''hOLA QJ HACE COM OESTAN CHICOS COMO LA ESTÁN
+ASDASDASDASDASDASDASDaaaa####@@~@~23'''
+
+var_1 = 'hola \
+chicos'
+
+var_int_pos = 10 
+var_int_neg = -10 
+
+#Función print()
+
+#print(var_int_neg)
+#print(var_1)
+
+#Tipo de dato: Flotante 
+
+var_float_pos = 2.5 
+var_float_neg = -2.5
+
+#print(var_float_pos)
+
+#Función int() , float()
+
+var_2 = 10 #Entero
+var_3 = 5.5 #Flotante
+
+var_4 = float(var_2)
+
+print(var_4)
+
+var_5 = int(var_3) #varible de tipo entera
+
+print(var_5)
+
+#¿Cómo saber de qué tipo de dato estamos hablando?
+
+n_1 = 1 
+n_2 = 2.2
+n_3 = "hola"
+n_4 = False
+
+#Método type()
+#print(type(n_1))
+#print(type(n_2))
+#print(type(n_3))
+#print(type(n_4))
+
+#Método dir()
+
+dat_1 = 20 
+dat_2 = 10.1
+#print(dir(dat_1))
+
+#print(dir(dat_2))
+
+
+#Tipo de dato Booleano 
+
+p_1 = True 
+
+p_2 = False
+
+string_1 = ''
+
+print(p_1)
+
+print(dir(p_2))
+
+
+
+
---
+++ C3.py
@@ -0,0 +1,108 @@
+#TIPO DE DATO LISTA O ARRAY
+'''ARRAY 
+O LISTA'''
+
+my_list = [123 , 'cesar', True , ['carlos'] , 9.9 ]
+
+#FUNCIÓN DIR()
+
+#print(dir(my_list))
+
+#Método append()
+#Añade UNO término u objeto a la lista inicial.
+
+#Antes
+#print(my_list)
+
+#print(my_list.append('jhomar'))
+
+#Después
+#print(my_list)
+
+#Método clear()
+
+#print(my_list.clear())
+#print(my_list)
+
+#Método count()
+
+my_list_2 = ['hola','hola','hola', 1,1,1 , True]
+
+#print(my_list_2.count(1))
+
+#Método extend()
+
+my_list_3 = ['carlos','cesar','jhomar','peru','mexico']
+
+#print(my_list_3.extend(['programacion','python']))
+#print(my_list_3)
+
+#Método index()
+#Indicarnos la posición del objeto.
+my_list_4 = [2,3,5,7,11, 'hola', 'mundo']
+
+#print(my_list_4)
+#print(my_list_4.index('hola'))
+
+#Método insert()
+
+my_list_5 = ['hola','estas','carlos']
+#Hola como estas carlos
+#print(my_list_5.insert(1,'como'))
+#print(my_list_5)
+
+#Método pop()
+#Elimina el último término 
+
+my_list_6 = [1 , 1, 2 ,3 ,5 ,8]
+
+my_list_6.pop()
+
+#print(my_list_6)
+
+my_list_6.append(8)
+
+#print(my_list_6)
+
+#print(dir(my_list_6))
+
+#Método remove()
+
+my_list_7 = ['susan','carlos','cesar','jhomar']
+
+#Antes
+#print(my_list_7)
+
+my_list_7.remove('carlos')
+
+#Después
+#print(my_list_7)
+
+
+#Método reverse()
+
+my_list_8 = [1 , 3, 5, 4, 6]
+
+#my_list_8.reverse()
+print(my_list_8)
+
+#Método sort()
+
+my_list_8.sort()
+
+print(my_list_8)
+
+#########################################
+
+print('#'*100)
+
+var_int = 10
+var_float = 5.5
+
+
+var_new_2 = int(var_float)
+
+print(var_new_2)
+
+
+
